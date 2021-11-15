4f7ab74f3d0632736d999a6a63a7e36c6cb38acb
jhun0601 <ngjhunnen@gmail.com>
2021-11-15 17:56:38 +0800

    add user model

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -5,21 +5,6 @@ const getCoordinatesByAddress = require("../util/location");
 
 const Place = require("../models/place");
 
-let PLACES = [
-  {
-    id: "p1",
-    title: "Hagdan-hagdan palayan",
-    description:
-      "The terraces are occasionally called the Eighth Wonder of the World. It is commonly thought that the terraces were built with minimal equipment, largely by hand. ",
-    location: {
-      lat: 16.9347,
-      lng: 121.1354,
-    },
-    address: "Nueva Vizcaya - Ifugao - Mountain Province Rd, Banaue, Ifugao",
-    creator: "1",
-  },
-];
-
 const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
   let place;
@@ -94,32 +79,51 @@ const createPlace = async (req, res, next) => {
   res.status(201).json({ place: createdPlace });
 };
 
-const updatePlaceById = (req, res, next) => {
+const updatePlaceById = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError("This is a required field", 422);
+    return next(new HttpError("This is a required field", 422));
   }
 
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
-  const updatePlace = { ...PLACES.find((p) => p.id === placeId) };
-  const placeIndex = PLACES.findIndex((p) => p.id === placeId);
-
-  updatePlace.title = title;
-  updatePlace.description = description;
-
-  PLACES[placeIndex] = updatePlace;
-
-  res.status(200).json({ place: updatePlace });
-};
-const deletePlaceById = (req, res, next) => {
-  const placeId = req.params.pid;
-
-  if (!PLACES.find((p) => p.id === place)) {
-    throw new HttpError("Could not find place.", 404);
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError("something went wrong. update failed", 500);
+    return next(error);
   }
-  PLACES = PLACES.filter((p) => p.id !== placeId);
+
+  place.title = title;
+  place.description = description;
+
+  try {
+    await place.save();
+  } catch (err) {
+    const error = new HttpError("something went wrong. could not save", 500);
+    return next(error);
+  }
+
+  res.status(200).json({ place: place.toObject({ getters: true }) });
+};
+const deletePlaceById = async (req, res, next) => {
+  const placeId = req.params.pid;
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError("could not find place", 500);
+    return next(error);
+  }
+
+  try {
+    await place.remove();
+  } catch (err) {
+    const error = new HttpError("could not find place", 500);
+    return next(error);
+  }
   res.status(200).json({ message: "Deleted Place" });
 };
 
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -2,48 +2,81 @@ const { v4: uuidv4 } = require("uuid");
 
 const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
+const User = require("../models/user");
+// let USERS = [
+//   {
+//     id: "1",
+//     name: "Jhunmark Ng",
+//     email: "ngjhunnen@gmail.com",
+//     password: "testtest",
+//   },
+// ];
 
-let USERS = [
-  {
-    id: "1",
-    name: "Jhunmark Ng",
-    email: "ngjhunnen@gmail.com",
-    password: "testtest",
-  },
-];
-
-const getUsers = (req, res, next) => {
-  res.json({ users: USERS });
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find({}, "-password");
+  } catch (err) {
+    const error = new HttpError("Fetching Users failed. Please try again", 500);
+    return next(error);
+  }
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError("This is a required field", 422);
+    return next(new HttpError("This is a required field", 422));
   }
-  const { name, email, password } = req.body;
-  const hasUser = USERS.find((u) => u.email === email);
-  if (hasUser) {
-    throw new HttpError("User already exist", 422);
+  const { name, email, password, places } = req.body;
+
+  let existingUser;
+
+  try {
+    const existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("Signup Failed. Please try again", 500);
+    return next(error);
   }
-  const createUser = {
-    id: uuidv4(),
+
+  if (existingUser) {
+    const error = new HttpError("User already exist", 422);
+    return next(error);
+  }
+
+  const createdUser = new User({
     name,
     email,
+    image:
+      "https://media-exp1.licdn.com/dms/image/C5603AQERX4Lks3DnTQ/profile-displayphoto-shrink_200_200/0/1619358015036?e=1642636800&v=beta&t=CfOwIVe26Qi5YlBapKHn5f9lHcaUy4H9EzP9dvmUG9Q",
     password,
-  };
+    places,
+  });
 
-  USERS.push(createUser);
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError("Signup failed. Please try again", 500);
+    return next(error);
+  }
 
-  res.status(200).json({ user: createUser });
+  res.status(200).json({ user: createdUser.toObject({ getters: true }) });
 };
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  const identifiedUser = USERS.find((u) => u.email === email);
+  let existingUser;
 
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError("Could not find user, Email/Password is invalid", 401);
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("Login Failed. Please try again", 500);
+    return next(error);
+  }
+
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError("Invalid Email/Password", 401);
+    return next(error);
   }
 
   return res.json({ message: "logged in" });
